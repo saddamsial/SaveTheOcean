f761d122132c46f35a32992cb4d28d7761ea078d
Anton Pashkevich <natoy7000@gmail.com>
2022-08-10 14:31:18 +0200

    Updated rotation Earth

--- Assets/_sto/Scripts/Earth/Rotation.cs
+++ Assets/_sto/Scripts/Earth/Rotation.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 public class Rotation : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private float _speedRotation = 5f;
+
+    [Header("Speed rotation")] [SerializeField]
+    private float _forceRotation = 100f;
+
+    [SerializeField] private float _speedRotationTransform = 16f;
     [SerializeField] private float _speedRotationToSelectLevel = 20f;
-    [SerializeField] private float _maxAngularVelocity = 14f;
+    [Space] [SerializeField] private float _maxAngularVelocity = 14f;
 
     private bool _isRotateToSelectLevel;
     private float _forceRotationY;
-    private bool _isMouseDown = false;
-    private IEnumerator _enumeratorStopRotation;
+ //   private bool _isMouseDown = false;
+ //   private IEnumerator _enumeratorStopRotationPhysics;
 
-    private void Start()
+    private void Awake()
     {
         _rigidbody.centerOfMass = Vector3.zero;
         _rigidbody.maxAngularVelocity = _maxAngularVelocity;
@@ -21,51 +25,81 @@ public class Rotation : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        if (IsButtonMouseDown())
         {
-            if (_enumeratorStopRotation != null)
-                StopCoroutine(_enumeratorStopRotation);
-            _enumeratorStopRotation = StopRotationPhysic();
-            StartCoroutine(_enumeratorStopRotation);
+            ResetForceAndVelocityRotation();
+            _rigidbody.isKinematic = true;
+            /*  if (_enumeratorStopRotationPhysics != null)
+                  StopCoroutine(_enumeratorStopRotationPhysics);
+              _enumeratorStopRotationPhysics = StopRotationPhysic();
+              StartCoroutine(_enumeratorStopRotationPhysics);*/
+        }
+
+        if (IsMouseButton())
+        {
+            TransformRotation();
+        }
+
+        if (IsButtonMouseUp())
+        {
+            _rigidbody.isKinematic = false;
+            RotatePhysics();
         }
     }
 
-    private void FixedUpdate()
+    private static bool IsMouseButton() => Input.GetMouseButton(0) || Input.touchCount > 0;
+
+    private bool IsButtonMouseDown() => Input.GetMouseButtonDown(0) ||
+                                        (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began);
+
+    private bool IsButtonMouseUp() => Input.GetMouseButtonUp(0) || (Input.touchCount > 0 &&
+                                                                    Input.GetTouch(0).phase is TouchPhase.Canceled
+                                                                        or TouchPhase.Ended);
+
+
+    /*   private void FixedUpdate()
+       {
+         if((Input.GetMouseButton(0) || Input.touchCount > 0) && !_isRotateToSelectLevel)
+         {
+           RotatePhysics();
+         }
+       }*/
+
+    private void TransformRotation()
     {
-      if((Input.GetMouseButton(0) || Input.touchCount > 0) && !_isRotateToSelectLevel)
-      {
-        Rotate();
-      }
+        var positionMouse = Input.GetAxis("Mouse X");
+        transform.RotateAround(transform.position, -transform.up,
+            Time.deltaTime * _speedRotationTransform * positionMouse);
     }
 
-    private IEnumerator StopRotationPhysic()
-    {
-        var timer = 0f;
-        while (timer < 0.1f)
-        {
-            timer += Time.deltaTime;
-            var mouseX = Input.GetAxis("Mouse X");
-            if (mouseX is > 0.1f or < -0.1f)
-            {
-                yield break;
-            }
+    /*   private IEnumerator StopRotationPhysic()
+       {
+           var timer = 0f;
+           while (timer < 0.08f)
+           {
+               timer += Time.deltaTime;
+               var mouseX = Input.GetAxis("Mouse X");
+               if (mouseX is > 0.1f or < -0.1f)
+               {
+                   yield break;
+               }
+   
+               yield return null;
+           }
+   
+           ResetForceAndVelocityRotation();
+       }*/
 
-            yield return null;
-        }
-        ResetForceAndVelocityRotation();
-    }
-
-    private void Rotate()
+    private void RotatePhysics()
     {
         var mouseX = Input.GetAxis("Mouse X");
 
-        HandlerForceAndVelocityRotation(mouseX);
-
-        _forceRotationY += _speedRotation * mouseX * Time.deltaTime;
+    //    HandlerForceAndVelocityRotation(mouseX);
+        _forceRotationY += _forceRotation * mouseX * Time.deltaTime;
         _rigidbody.AddTorque(transform.up * -_forceRotationY);
     }
 
-    private void HandlerForceAndVelocityRotation(float mouseX)
+ /*   private void HandlerForceAndVelocityRotation(float mouseX)
     {
         if ((mouseX > 0 && _rigidbody.angularVelocity.y > 0) ||
             (mouseX < 0 && _rigidbody.angularVelocity.y < 0)
@@ -74,7 +108,7 @@ public class Rotation : MonoBehaviour
 
         if (_rigidbody.angularVelocity.y is < 0.1f and > -0.1f)
             ResetForceRotation();
-    }
+    }*/
 
     public void RotateToSelectLevel(float angle)
     {
@@ -99,8 +133,8 @@ public class Rotation : MonoBehaviour
                 Time.deltaTime * mult * _speedRotationToSelectLevel);
         }
 
-        _rigidbody.isKinematic = false;
         _isRotateToSelectLevel = false;
+        _rigidbody.isKinematic = false;
     }
 
     private void ResetForceAndVelocityRotation()
@@ -110,10 +144,13 @@ public class Rotation : MonoBehaviour
     }
 
     private void ResetForceRotation() => _forceRotationY = 0;
-
-    public void SetSpeedRotation(float speed) => _speedRotation = speed;
-
+    public void SetSpeedRotationTransform(float speed) => _speedRotationTransform = speed;
+    public void SetForceRotation(float force) => _forceRotation = force;
     public void SetMaxAngularVelocity(float velocity) => _rigidbody.maxAngularVelocity = velocity;
-
     public void SetAngularDrag(float drag) => _rigidbody.angularDrag = drag;
+    
+    public float GetSpeedRotation() => _speedRotationTransform;
+    public float GetForceRotation() => _forceRotation;
+    public float GetMaxAngularVelocity() => _rigidbody.maxAngularVelocity;
+    public float GetAngularDrag() => _rigidbody.angularDrag;
 }
--- Assets/_sto/Scripts/Earth/UIDebug.cs
+++ Assets/_sto/Scripts/Earth/UIDebug.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,9 +6,12 @@ using UnityEngine.UI;
 public class UIDebug : MonoBehaviour
 {
     [SerializeField] private Rotation _rotationEarth; 
-    [Header("Speed Rotation")]
+    [Header("Speed Rotation Transform")]
     [SerializeField] private TextMeshProUGUI _speedRotationText;
     [SerializeField] private Slider _speedRotationSlider;
+    [Header("Force Rotation")]
+    [SerializeField] private TextMeshProUGUI _forceRotationText;
+    [SerializeField] private Slider _forceRotationSlider;
     [Header("Max angular Velocity")]
     [SerializeField] private TextMeshProUGUI _maxAngularVelocityText;
     [SerializeField] private Slider _maxAngularVelocitySlider;
@@ -15,11 +19,28 @@ public class UIDebug : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _angularDragText;
     [SerializeField] private Slider _angularDragSlider;
 
+    private void Start()
+    {
+        SetSpeedRotation(_rotationEarth.GetSpeedRotation() / 10);
+        SetForceRotation(_rotationEarth.GetForceRotation() / 1000);
+        SetMaxAngularVelocity(_rotationEarth.GetMaxAngularVelocity());
+        SetAngularDrag(_rotationEarth.GetAngularDrag());
+    }
+
     public void SetSpeedRotation(float speed)
     {
-        _speedRotationSlider.value = speed; 
-        _speedRotationText.text = "Speed Rotation:" + speed; 
-        _rotationEarth.SetSpeedRotation(speed);
+        _speedRotationSlider.value = speed;
+        var value = speed * 10; 
+        _speedRotationText.text = "Speed Rotation Transform:" + value ; 
+        _rotationEarth.SetSpeedRotationTransform(value);
+    }
+    
+    public void SetForceRotation(float speed)
+    {
+        _forceRotationSlider.value = speed;
+        var value = speed * 1000; 
+        _forceRotationText.text = "Force Rotation:" + value ; 
+        _rotationEarth.SetForceRotation(value);
     }
     
     public void SetMaxAngularVelocity(float value)
