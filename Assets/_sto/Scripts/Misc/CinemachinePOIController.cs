2b4b2261d04c2989390bcb539223608d06185ac8
msuchy82 <theunspokenterror@gmail.com>
2022-09-02 14:06:02 +0200

    Cinemachine added

---
+++ Assets/_sto/Scripts/Misc/CinemachinePOIController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameLib.CameraSystem;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineTargetGroup))]
+public class CinemachinePOIController : MonoBehaviour
+{
+    CinemachineTargetGroup targetGroup = null;
+
+    private void Awake() {
+        targetGroup = GetComponent<CinemachineTargetGroup>();
+    }
+    private void OnEnable() {
+        CameraPOI.onActivated += RegisterPOI;
+        CameraPOI.onDeactivated += UnregisterPOI;
+    }
+    private void OnDisable() {
+        CameraPOI.onActivated -= RegisterPOI;
+        CameraPOI.onDeactivated -= UnregisterPOI;        
+    }
+
+    void RegisterPOI(CameraPOI sender){
+        targetGroup.AddMember(sender.transform, 1f,0f);
+    }
+    void UnregisterPOI(CameraPOI sender){
+        targetGroup.RemoveMember(sender.transform);
+    }
+
+}
