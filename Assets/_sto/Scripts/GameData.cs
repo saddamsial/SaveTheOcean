b3f119531a0b032e37c9574cfda0ed85279c851e
msuchy82 <theunspokenterror@gmail.com>
2022-09-19 16:47:37 +0200

    balance progress - levels

--- Assets/_sto/Scripts/GameData.cs
+++ Assets/_sto/Scripts/GameData.cs
@@ -24,6 +24,7 @@ public class GameData : ScriptableObject
     Cat_00_lvl3 = 3,
     Cat_00_lvl4 = 4,
     Cat_00_lvl5 = 5,
+    Cat_00_lvl6 = 6,
 
     Cat_01_lvl0 = 10,
     Cat_01_lvl1 = 11,
@@ -31,6 +32,7 @@ public class GameData : ScriptableObject
     Cat_01_lvl3 = 13,
     Cat_01_lvl4 = 14,
     Cat_01_lvl5 = 15,
+    Cat_01_lvl6 = 16,
 
     Cat_02_lvl0 = 20,
     Cat_02_lvl1 = 21,
@@ -38,13 +40,15 @@ public class GameData : ScriptableObject
     Cat_02_lvl3 = 23,
     Cat_02_lvl4 = 24,
     Cat_02_lvl5 = 25,
+    Cat_02_lvl6 = 26,
 
-    // Cat_03_lvl0 = 30,
-    // Cat_03_lvl1 = 31,
-    // Cat_03_lvl2 = 32,
-    // Cat_03_lvl3 = 33,
-    // Cat_03_lvl4 = 34,
-    // Cat_03_lvl5 = 35,
+    Cat_03_lvl0 = 30,
+    Cat_03_lvl1 = 31,
+    Cat_03_lvl2 = 32,
+    Cat_03_lvl3 = 33,
+    Cat_03_lvl4 = 34,
+    Cat_03_lvl5 = 35,
+    Cat_03_lvl6 = 36,
 
     // Cat_04_lvl0 = 40,
     // Cat_04_lvl1 = 41,
