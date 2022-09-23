4f866e495ee16f39ad1a005578c2c628b7581336
msuchy82 <theunspokenterror@gmail.com>
2022-09-23 11:31:18 +0200

    Garbage Pile progress - animation, fx, max count clamp

--- Assets/_sto/Scripts/Misc/GarbagePile.cs
+++ Assets/_sto/Scripts/Misc/GarbagePile.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using GameLib;
 
 public class GarbagePile : MonoBehaviour
 {
+    public static System.Action<Transform> onGarbageRemoved;
     [SerializeField] Vector2 pileSize = Vector2.one;
+    [SerializeField] int maxPileCount = 100;
+    int invisibleItemCount = 0;
     [SerializeField] GameObject elementPrefab = null;
-    [SerializeField] Vector2 sizeRandom = Vector2.up; 
+    [SerializeField] Vector2 elementSizeRandom = Vector2.up; 
+    [SerializeField] Vector2 areaClamp = new Vector2(1, 5);
+    float _areaMultiplier(int itemCount) => Mathf.Clamp((Mathf.RoundToInt(itemCount/100f) + 1), areaClamp.x, areaClamp.y);
+    [SerializeField] ParticleSystem trashPopFX = null;
     
     Queue<GameObject> _pileQueue = new Queue<GameObject>();
+    float _pileRadius = 1f;
     
+    Vector3 GetRandomPointInArea(float radius, float height) => new Vector3(Random.Range(-radius, radius), Random.Range(0f, height), Random.Range(-radius, radius));
+
     private void OnEnable() {
         Level.onStart += GeneratePile;
         Level.onUnderwaterSpawn += PopTrash;
@@ -27,28 +37,39 @@ public class GarbagePile : MonoBehaviour
 
     public void GeneratePile(int quantity){
         _pileQueue = new Queue<GameObject>();
+        _pileRadius = _areaMultiplier(quantity) * pileSize.x;
 
-        for (int i = 0; i < quantity; i++){
-            var spawnOffset = Random.insideUnitSphere * pileSize.x;
-            spawnOffset = new Vector3(spawnOffset.x, Mathf.Abs(spawnOffset.y * pileSize.y), spawnOffset.z);
+        for (int i = 0; i < Mathf.Clamp(quantity,0 ,maxPileCount); i++){
+            var spawnOffset = GetRandomPointInArea(_pileRadius, pileSize.y);
 
             var newSpawn = Instantiate(
                 elementPrefab, 
                 transform.position + spawnOffset,
                 Quaternion.LookRotation(Random.onUnitSphere),
                 this.transform);
-                newSpawn.transform.localScale = Vector3.one * Random.Range(sizeRandom.x, sizeRandom.y);
+                newSpawn.transform.localScale = Vector3.one * Random.Range(elementSizeRandom.x, elementSizeRandom.y);
             _pileQueue.Enqueue(newSpawn);
         }
+        invisibleItemCount = Mathf.Abs(quantity - maxPileCount);
     }
 
     public void PopTrash(){
-        if (_pileQueue.TryDequeue(out GameObject lastObject))
-            lastObject.gameObject.SetActive(false);
+        Debug.Log("Trash Pile | " + invisibleItemCount + " | " + _pileQueue.Count);
+
+        if (invisibleItemCount-- > 0) {
+            trashPopFX?.PlayAtPosition(transform.position + GetRandomPointInArea(pileSize.x * _pileRadius, pileSize.y), Vector3.up);
+            return;
+        }
+
+        if (!_pileQueue.TryDequeue(out GameObject lastObject)) return;
+
+        lastObject.gameObject.SetActive(false);
+        onGarbageRemoved?.Invoke(lastObject.transform);
+        trashPopFX?.PlayAtPosition(lastObject.transform.position, Vector3.up);
     }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
-        GameLib.GizmosExtensions.DrawWireCircle(transform.position, pileSize.x, transform.up);
+        GameLib.GizmosExtensions.DrawWireCircle(transform.position, _pileRadius, transform.up);
     }
 }
